43c773fa9c98cdf426f5fd50972eab0fca9c2c06
Mathias Lipke <Mathias.Lipke@infomotion.de>
2023-03-17 10:34:05 +0000

    Fix Resource Group for Machine Learning Workspace

--- modules/analytics/machine_learning/main.tf
+++ modules/analytics/machine_learning/main.tf
@@ -3,12 +3,10 @@ locals {
     "module" = basename(abspath(path.module))
   }
   tags = merge(var.base_tags, local.module_tag, try(var.settings.tags, null))
-  resource_group = coalesce(
-    try(var.resource_groups[var.client_config.landingzone_key][var.settings.resource_group_key], null),
-    try(var.resource_groups[var.settings.lz_key][var.settings.resource_group_key], null),
-    try(var.resource_groups[var.client_config.landingzone_key][var.settings.resource_group.key], null),
-    try(var.resource_groups[var.settings.resource_group.lz_key][var.settings.resource_group.key], null)
-  )
+
+  location            = can(var.settings.location) ? var.settings.location : var.resource_groups[try(var.settings.resource_group.lz_key, var.client_config.landingzone_key)][try(var.settings.resource_group.key, var.settings.resource_group_key)].location
+  resource_group_name = can(var.settings.resource_group_name) || can(var.settings.resource_group.name) ? try(var.settings.resource_group_name, var.settings.resource_group.name) : var.resource_groups[try(var.settings.resource_group.lz_key, var.client_config.landingzone_key)][try(var.settings.resource_group_key, var.settings.resource_group.key)].name
+
 }
 
 terraform {
