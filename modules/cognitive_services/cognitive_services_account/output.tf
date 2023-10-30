150bd4d529d6468d255619ae90e02e01d2e3ac80
Emin Bozkurt <ebozkurt@infomotion.de>
2023-10-30 12:21:32 +0100

    add output for cognitive services, access keys

--- modules/cognitive_services/cognitive_services_account/output.tf
+++ modules/cognitive_services/cognitive_services_account/output.tf
@@ -7,3 +7,13 @@ output "endpoint" {
   description = "The endpoint used to connect to the Cognitive Service Account."
   value       = azurerm_cognitive_account.service.endpoint
 }
+
+output "primary_access_key" {
+  description = "The primary access key used to connect to the Cognitive Service Account."
+  value       = azurerm_cognitive_account.service.primary_access_key
+}
+
+output "primary_access_key" {
+  description = "The secondary access key used to connect to the Cognitive Service Account."
+  value       = azurerm_cognitive_account.service.secondary_access_key
+}
