470c76379bb8b14be4d68dd8177eafe2e51a0e26
Emin Bozkurt <ebozkurt@infomotion.de>
2023-10-30 14:46:13 +0100

    add sensitive   = true to output

--- modules/cognitive_services/cognitive_services_account/output.tf
+++ modules/cognitive_services/cognitive_services_account/output.tf
@@ -11,9 +11,11 @@ output "endpoint" {
 output "primary_access_key" {
   description = "The primary access key used to connect to the Cognitive Service Account."
   value       = azurerm_cognitive_account.service.primary_access_key
+  sensitive   = true
 }
 
 output "secondary_access_key" {
   description = "The secondary access key used to connect to the Cognitive Service Account."
   value       = azurerm_cognitive_account.service.secondary_access_key
+  sensitive   = true
 }
